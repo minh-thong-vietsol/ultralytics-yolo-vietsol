728e0c425618e92b2d814c78408ddb859048ae9f
ToyzZone <163508597+minh-thong-vietsol@users.noreply.github.com>
2024-03-31 20:15:30 +0700

    update output path

--- test.py
+++ test.py
@@ -2,5 +2,10 @@ from ultralytics import YOLO
 
 model = YOLO('mthong_detect/yolov9_vietsol/models/model_weights/collection/yolov9c.pt')
 
-model.info(detailed=True)
+source = 'mthong_detect/yolov9_vietsol/data/vinfast_data/Ngay3-720p.mp4'
+max_det = 1000
+save = True
+save_txt = True
+
+model.predict(source = source, max_det = max_det, save = save, save_txt = save_txt)
 

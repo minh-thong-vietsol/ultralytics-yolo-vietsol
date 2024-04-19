07fdb166b14656158deb0121291b42f03dae83d1
ToyzZone <163508597+minh-thong-vietsol@users.noreply.github.com>
2024-04-19 16:08:45 +0700

    test exporting

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
+from os import path
 from unittest import result
 from ultralytics import YOLO
 
 model = YOLO('ultralytics/cfg/models/v3/yolov3.yaml')
 
-results = model.train(data='coco128.yaml', epochs=3)
+#Valid formats are 
+#('torchscript', 'onnx', 'openvino', 'engine', 'coreml', 'saved_model', 'pb', 'tflite', 'edgetpu', 'tfjs', 'paddle', 'ncnn')
+
+model.export(format='onnx')
 
 
 

2aeede2de97e3f1b89f6374cfa2a1b1db837859e
ToyzZone <163508597+minh-thong-vietsol@users.noreply.github.com>
2024-04-19 08:17:39 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
+from unittest import result
 from ultralytics import YOLO
 
-model = YOLO('mthong_detect/yolov9_vietsol/models/model_weights/collection/yolov9c.pt')
+model = YOLO('ultralytics/cfg/models/v3/yolov3.yaml')
 
-source = 'mthong_detect/yolov9_vietsol/data/vinfast_data/Ngay3-720p.mp4'
-max_det = 1000
-save = True
-save_txt = True
+results = model.train(data='coco128.yaml', epochs=3)
 
-model.predict(source = source, max_det = max_det, save = save, save_txt = save_txt)
+
 
